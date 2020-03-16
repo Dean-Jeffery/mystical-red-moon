3c0e190b9976c5baa1e6c1e96e1382fceddb586f
Dean Jeffery <itsdeanrj@gmail.com>
2020-03-16 14:56:00 +0000

    html for index + css

--- wp-content/themes/MysticalRedMoon/index.php
+++ wp-content/themes/MysticalRedMoon/index.php
@@ -1,31 +1,36 @@
 <?php get_header(); ?>
 <main id="content">
-    <div class="feature">
-
-        <h2>Mystical Tarot</h2>
-        <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <img src="https://images.unsplash.com/photo-1494788185066-84d048a0115a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="profile-img">
-
+<div class="feature">
+    <div class="two-thirds">
+      <h2>Mystical Tarot</h2>
+      <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <button type="button" name="button">Read more</button>
     </div>
-    <div class="red-segment">
-        <h3>Recent Blogs</h3>
-        <div>
-            <h4>blog 1</h4>
-            <p>this is some text.</p>
-            <button>button</button>
-        </div>
-        <div>
-            <h4>blog 2</h4>
-            <p>this is some text.</p>
-            <button>button</button>
-        </div>
-        <div>
-            <h4>blog 3</h4>
-            <p>this is some text.</p>
-            <button>button</button>
-        </div>
+    <div class="one-thirds">
+      <img src="https://image.shutterstock.com/image-photo/happy-cheerful-young-woman-wearing-260nw-613759379.jpg" alt="profile-pic">
     </div>
+  </div>
+    <div class="red-container">
+  <div class="thirds">
+    <div class="one-thirds">
+      <h3>Recent blog #1</h3>
+      <p>To read about this blog click the button below.</p>
+      <button type="button" name="button">Button:)</button>
+    </div>
+    <div class="one-thirds">
+      <h3>Recent blog #2</h3>
+      <p>To read about this blog click the button below.</p>
+      <button type="button" name="button">Button:)</button>
+    </div>
+    <div class="one-thirds">
+      <h3>Recent blog #3</h3>
+      <p>To read about this blog click the button below.</p>
+      <button type="button" name="button">Button:)</button>
+    </div>
+  </div>
+</div>
 </main>
 <?php get_footer(); ?>
