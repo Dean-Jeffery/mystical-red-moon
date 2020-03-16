f9bae3fbbd4df16b48ea2d0d4dd77d267c709540
Dean Jeffery <itsdeanrj@gmail.com>
2020-03-16 14:59:52 +0000

    update footer

--- wp-content/themes/MysticalRedMoon/footer.php
+++ wp-content/themes/MysticalRedMoon/footer.php
@@ -1,7 +1,32 @@
 </div>
+
 <footer id="footer">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+  <p>Mystical Red Moon</p>
+
+<div class="thirds">
+
+
+<div class="one-thirds">
+  <div class="contact">
+    <p>555 5555</p>
+    <p>email@mysticalRedMoon.com</p>
+  </div>
+</div>
+<div class="one-thirds">
+  <div id="copyright">
+  &copy; <?php echo esc_html(date_i18n(__('Y', 'mystical Red Moon'))); ?> <?php echo esc_html(get_bloginfo('name')); ?>
+  </div>
+</div>
+<div class="one-thirds">
+
+  <p>nav nav nav nav nav</p>
+
+</div>
+
+
+</div>
+<div>
+  <p>Website by <a href="http://deanjeffery.co.uk" target="_blank">Dean Jeffery</a></p>
 </div>
 </footer>
 </div>
