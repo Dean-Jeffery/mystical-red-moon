fb0d7aa84830c911bb8879df9213a48f285de791
Dean Jeffery <itsdeanrj@gmail.com>
2020-04-02 11:05:49 +0100

    update wordpress

--- wp-content/themes/MysticalRedMoon/footer.php
+++ wp-content/themes/MysticalRedMoon/footer.php
@@ -1,35 +1,36 @@
 </div>
 
 <footer id="footer">
-  <p>Mystical Red Moon</p>
+    <p>Mystical Red Moon</p>
 
-<div class="thirds">
-
-
-<div class="one-thirds">
-  <div class="contact">
-    <p>555 5555</p>
-    <p>email@mysticalRedMoon.com</p>
-  </div>
-</div>
-<div class="one-thirds">
-  <div id="copyright">
-  &copy; <?php echo esc_html(date_i18n(__('Y', 'mystical Red Moon'))); ?> <?php echo esc_html(get_bloginfo('name')); ?>
-  </div>
-</div>
-<div class="one-thirds">
-
-  <p>nav nav nav nav nav</p>
-
-</div>
+    <div class="thirds">
 
 
-</div>
-<div>
-  <p>Website by <a href="http://deanjeffery.co.uk" target="_blank">Dean Jeffery</a></p>
-</div>
+        <div class="one-thirds">
+            <div class="contact">
+                <p>555 5555</p>
+                <p>email@mysticalRedMoon.com</p>
+            </div>
+        </div>
+        <div class="one-thirds">
+            <div id="copyright">
+                &copy; <?php echo esc_html(date_i18n(__('Y', 'mystical Red Moon'))); ?> <?php echo esc_html(get_bloginfo('name')); ?>
+            </div>
+        </div>
+        <div class="one-thirds">
+
+            <p>nav nav nav nav nav</p>
+
+        </div>
+
+
+    </div>
+    <div>
+        <p>Website by <a href="http://deanjeffery.co.uk" target="_blank">Dean Jeffery</a></p>
+    </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
 </body>
+
 </html>
