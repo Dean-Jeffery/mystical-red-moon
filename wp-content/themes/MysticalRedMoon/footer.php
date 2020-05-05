189cd35b7f430b2ffddf8c59f99d3b15077c077f
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-05 16:52:36 +0100

    change header html + more
    -change header from pulling content from wp backend to static
    -add shadow to footer
    -start formatting of footer with maxwidth flexbox etc

--- wp-content/themes/MysticalRedMoon/footer.php
+++ wp-content/themes/MysticalRedMoon/footer.php
@@ -5,23 +5,40 @@
 
 </div>
 <footer id="footer">
+<h5>Mystical Red Moon</h5>
+
     <div class="wrapper">
-        <h5>Mystical Red Moon</h5>
-        <div>
+        <div class=""></div>
             <div>
                 <div class="contact">
                     <p>hello@redmoontarot.com</p>
                 </div>
             </div>
             <div>
-                <p>nav nav nav nav nav</p>
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">Blog</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                    <li>
+                        <a href="">Shop</a>
+                    </li>
+                </ul>
             </div>
-        </div>
-        <div class="shameless-plug">
-            <p>Website by <a href="http://deanjeffery.co.uk" target="_blank">Dean Jeffery</a></p>
-        </div>
     </div>
+    
 </footer>
+<div class="shameless-plug">
+    <p>Website by <a href="http://deanjeffery.co.uk" target="_blank">Dean Jeffery</a></p>
+</div>
 </div>
 <?php wp_footer(); ?>
 </body>
--- wp-content/themes/MysticalRedMoon/header.php
+++ wp-content/themes/MysticalRedMoon/header.php
@@ -10,21 +10,15 @@
   <header id="header">
   <div class="wrapper">
     <div id="site-title">
-        <?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
-    echo '<h1>';
-} ?>
-        <a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_html(get_bloginfo('name')); ?>" rel="home"><?php echo esc_html(get_bloginfo('name')); ?></a>
-        <?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
-    echo '</h1>';
-} ?>
+        <h1><a href="Home">Red Moon Tarot</a></h1>
     </div>
     <div class="nav-bar">
     <nav>
       <ul>
         <li><a href="index">Home</a></li>
         <li><a href="about">About</a></li>
-        <li><a href="contact">Contact</a></li>
         <li><a href="Blog">Blog</a></li>
+        <li><a href="contact">Contact</a></li>
         <li><a href="Shop">Shop</a></li>
       </ul>
     </nav>
