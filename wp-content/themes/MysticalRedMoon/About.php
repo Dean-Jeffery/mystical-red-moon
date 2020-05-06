6550b0d7c6bd3df6da3aa2107ca1a7faa8ccdcc8
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-06 15:32:29 +0100

    forgot to commit today..... bunch of things
    -about page --- add more temp text
    -footer.php --- finished(unsure on bg color)
    -style.css --- changed fallback font for h1, increase letterspacing+lineheight, alter padding/margin accross system design

--- wp-content/themes/MysticalRedMoon/About.php
+++ wp-content/themes/MysticalRedMoon/About.php
@@ -8,13 +8,21 @@
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                 ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
+            <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
         </div>
         <div class="one-thirds">
             <img src="https://image.shutterstock.com/image-photo/happy-cheerful-young-woman-wearing-260nw-613759379.jpg" alt="profile-pic">
-        </div>
-    </div>
-    <div class="red-container">
-        <div class="thirds">
         </div>
     </div>
 </main>
--- wp-content/themes/MysticalRedMoon/footer.php
+++ wp-content/themes/MysticalRedMoon/footer.php
@@ -1,44 +1,45 @@
 </div>
-<div>
+<div class="wrapper">
     <!-- Email signup -->
-    <h4>Email signup</h4>
+    <h4>Join Our Mailing List</h4>
 
 </div>
 <footer id="footer">
-<h5>Mystical Red Moon</h5>
 
     <div class="wrapper">
-        <div class=""></div>
-            <div>
-                <div class="contact">
-                    <p>hello@redmoontarot.com</p>
-                </div>
+    <h5>Red Moon Tarot</h5>
+    <div class="footer">
+        <div>
+            <div class="contact">
+                <p>hello@redmoontarot.com</p>
             </div>
-            <div>
-                <ul>
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li>
-                        <a href="">About</a>
-                    </li>
-                    <li>
-                        <a href="">Blog</a>
-                    </li>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                    <li>
-                        <a href="">Shop</a>
-                    </li>
-                </ul>
-            </div>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Blog</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+                <li>
+                    <a href="">Shop</a>
+                </li>
+            </ul>
+        </div>
+        </div>
     </div>
-    
-</footer>
-<div class="shameless-plug">
+    <div class="shameless-plug">
     <p>Website by <a href="http://deanjeffery.co.uk" target="_blank">Dean Jeffery</a></p>
-</div>
+</div> 
+</footer>
+
 </div>
 <?php wp_footer(); ?>
 </body>
