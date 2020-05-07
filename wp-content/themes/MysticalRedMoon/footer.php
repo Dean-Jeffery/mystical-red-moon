4c21c09c25fbfe282ba07995ea72b986c80fc83a
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-07 20:40:59 +0100

    another day of forgetting to push
    - this email sign up is causing all sorts of issues...
    -

--- wp-content/themes/MysticalRedMoon/footer.php
+++ wp-content/themes/MysticalRedMoon/footer.php
@@ -1,38 +1,41 @@
 </div>
 <div class="wrapper">
     <!-- Email signup -->
-    <h4>Join Our Mailing List</h4>
-
+    <form action="">
+        <h4>Join Our Mailing List</h4>
+        <input type="text" placeholder="Enter Email" name="email" required>
+        <button type="submit" class="button">Signup</button>
+    </form>
 </div>
 <footer id="footer">
 
     <div class="wrapper">
     <h5>Red Moon Tarot</h5>
-    <div class="footer">
-        <div>
-            <div class="contact">
-                <p>hello@redmoontarot.com</p>
+        <div class="footer">
+            <div>
+                <div class="contact">
+                    <p>hello@redmoontarot.com</p>
+                </div>
             </div>
-        </div>
-        <div>
-            <ul>
-                <li>
-                    <a href="">Home</a>
-                </li>
-                <li>
-                    <a href="">About</a>
-                </li>
-                <li>
-                    <a href="">Blog</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
-                <li>
-                    <a href="">Shop</a>
-                </li>
-            </ul>
-        </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+                    <li>
+                        <a href="">Blog</a>
+                    </li>
+                    <li>
+                        <a href="">Contact</a>
+                    </li>
+                    <li>
+                        <a href="">Shop</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class="shameless-plug">
