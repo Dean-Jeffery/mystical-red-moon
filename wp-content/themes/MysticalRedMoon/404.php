fc876a941bb1f70fbfa1399175fdbc157a4b1fb4
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-12 13:01:14 +0100

    trynna figure out a single blog template

--- wp-content/themes/MysticalRedMoon/404.php
+++ wp-content/themes/MysticalRedMoon/404.php
@@ -5,8 +5,7 @@
 <h1 class="entry-title"><?php esc_html_e( 'Not Found', 'blankslate' ); ?></h1>
 </header>
 <div class="entry-content">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
+
 </div>
 </article>
 </main>
---
+++ wp-content/themes/MysticalRedMoon/blog-page.php
@@ -0,0 +1,29 @@
+<?php /* Template Name: About */ ?>
+<?php get_header(); ?>
+<main id="content">
+    <div class="feature about-feature">
+        <div class="two-thirds">
+            <h2>About me</h2>
+            <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </div>
+        <div class="one-thirds">
+            <img src="https://image.shutterstock.com/image-photo/happy-cheerful-young-woman-wearing-260nw-613759379.jpg" alt="profile-pic">
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
