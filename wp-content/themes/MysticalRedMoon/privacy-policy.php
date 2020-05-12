dc5bfbfa59673aa4983ff6e929e79cc3fecfcd7a
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-12 20:20:04 +0100

    add privacy policy file, small tweaks
    - changed height of buttons

---
+++ wp-content/themes/MysticalRedMoon/privacy-policy.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Privacy Policy */ ?>
+<?php get_header(); ?>
+<main id="content">
+    <div class="feature about-feature">
+        <h2>Privacy policy</h2>
+    </div>
+</main>
+<?php get_footer(); ?>
