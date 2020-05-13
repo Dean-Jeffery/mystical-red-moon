190418e895f5c9cb6931fe9d7e628a913a60b419
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-13 08:49:49 +0100

    figured out templates for blog posts

--- wp-content/themes/MysticalRedMoon/single.php
+++ wp-content/themes/MysticalRedMoon/single.php
@@ -1,5 +1,24 @@
 <?php /* Template Name: Template for blog posts */ ?>
 <?php get_header(); ?>
-<main>
-</main>
+
+
+<div id="primary" class="site-content feature">
+  <div id="content" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+      <div class="post-title">
+        <?php the_post_thumbnail(); ?>
+        <h2 class="entry-title"><?php the_title(); ?></h2>
+      </div>
+
+      <div class="entry-content">
+        <?php the_content(); ?>
+      </div><!-- .entry-content -->
+
+    <?php endwhile; // end of the loop. ?>
+
+  </div><!-- #content -->
+</div><!-- #primary -->
+
+
 <?php get_footer(); ?>
