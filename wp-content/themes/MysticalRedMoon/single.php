beab183e829277792ce61fd947adcf5067a62678
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-13 13:55:58 +0100

    update yoast

--- wp-content/themes/MysticalRedMoon/single.php
+++ wp-content/themes/MysticalRedMoon/single.php
@@ -6,14 +6,15 @@
   <div id="content" role="main">
 
     <?php while ( have_posts() ) : the_post(); ?> <!-- Pull blog content -->
-      <div class="post-title">
-        <?php the_post_thumbnail(); ?>
-        <h2 class="entry-title"><?php the_title(); ?></h2>
-      </div>
+      
+    <div class="post-title">
+      <?php the_post_thumbnail(); ?>
+      <h2 class="entry-title"><?php the_title(); ?></h2>
+    </div>
 
-      <div class="entry-content">
-        <?php the_content(); ?>
-      </div><!-- .entry-content -->
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div><!-- .entry-content -->
 
     <?php endwhile; // end of the loop. ?>
 
