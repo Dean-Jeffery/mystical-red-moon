8a58adc695f3f0358e2db65877bf979e39901df2
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-14 14:13:23 +0100

    use working recent blogs segment on blog pages

--- wp-content/themes/MysticalRedMoon/index.php
+++ wp-content/themes/MysticalRedMoon/index.php
@@ -16,50 +16,25 @@
   <div class="red-container">
     <div class="wrapper">
       <h2>Recent Blogs</h2>
-      <!-- <div class="thirds">
-      <div class="one-thirds">
-      <h3>Recent blog #1</h3>
-      <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <a href="about.php" class="button">Read more</a>
-      </div>
-      <div class="one-thirds">
-      <h3>Recent blog #2</h3>
-      <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <a href="about.php" class="button">Read more</a>
-      </div>
-      <div class="one-thirds">
-      <h3>Recent blog #3</h3>
-      <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <a href="about.php" class="button">Read more</a>
-      </div> -->
-
       <div class="thirds">
         <!-- // Define WP Query Parameters -->
         <?php $the_query = new WP_Query( 'posts_per_page=3' ); ?>
-
         <!-- // Start WP Query -->
         <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
         <div class="one-thirds">
           <!-- // Display the Post Title with Hyperlink -->
           <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-
           <!-- // Display the Post Excerpt -->
           <p><?php the_excerpt(__('(more…)')); ?></p>
-
           <a href="<?php the_permalink() ?>" class="button">Read more</a>
         </div>
-
       <!-- // Repeat the process and reset once it hits the limit -->
       <?php 
       endwhile;
       wp_reset_postdata();
       ?>
       </div>
-
-
-
     </div>
-  </div>
   </div>
 </main>
 <?php get_footer(); ?>
