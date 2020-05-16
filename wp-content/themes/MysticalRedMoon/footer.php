93502724e2cd36f8593d5cd8011a2eba99261d06
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-16 12:58:27 +0100

    fix footer nav

--- wp-content/themes/MysticalRedMoon/footer.php
+++ wp-content/themes/MysticalRedMoon/footer.php
@@ -18,26 +18,8 @@
                 </div>
             </div>
             <div>
-                <ul>
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li>
-                        <a href="">About</a>
-                    </li>
-                    <li>
-                        <a href="">Blog</a>
-                    </li>
-                    <li>
-                        <a href="">Resources</a>
-                    </li>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                    <!-- <li>
-                        <a href="">Shop</a>
-                    </li> -->
-                </ul>
+            <?php wp_nav_menu( array( 'theme_location' => 'Header-menu' ) );?>
+
             </div>
         </div>
     </div>
