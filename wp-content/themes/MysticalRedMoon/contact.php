58aca6696b855724fb777e6e0970b1556be4a570
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-18 13:13:51 +0100

    style contact form

--- wp-content/themes/MysticalRedMoon/contact.php
+++ wp-content/themes/MysticalRedMoon/contact.php
@@ -2,15 +2,13 @@
 <?php get_header(); ?>
 <main id="content">
     <div class="feature ">
-        <h2>Get in touch...</h2>
-
-
-        
-       <?php echo do_shortcode('[contact-form-7 id="89" title="Contact form 1"]');?>
-        
-        
-
-
+        <div class="one-half">
+            <h2>Get in touch...</h2>
+            <p>dana@redmoontarot.co.uk</p>
+        </div>
+        <div class="one-half">
+            <?php echo do_shortcode('[contact-form-7 id="89" title="Contact form 1"]');?>
+        </div>
     </div>
 </main>
 <?php get_footer(); ?>
