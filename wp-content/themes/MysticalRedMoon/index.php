8b0baf6571cfa6f01e117d414bc7ea53218d2b2c
Dean Jeffery <itsdeanrj@gmail.com>
2020-05-24 03:12:01 +0100

    whole bunch of stuff
    - DO NOT REVERT BEFORE THIS
    
    i "think" ive got the client inputted content sorted for homepage and about
    - and some other fixes
    
    -code needs cleaned up

--- wp-content/themes/MysticalRedMoon/index.php
+++ wp-content/themes/MysticalRedMoon/index.php
@@ -4,18 +4,20 @@
 
 
   <div class="feature">
-    <div class="two-thirds">
-      <h2>Mystical Tarot</h2>
-      <p>This is some body text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-      <a href="about.php" class="button">Read more</a>
+        <div class="full-width">
+            <!-- pull content for about, probably not the most reliable way to do it but it works -->
+        <?php while ( have_posts() ) : the_post(); ?> <!-- Pull about content -->
+        <div class="title">
+            <?php the_post_thumbnail(); ?>
+            <h2 class="entry-title h2-no-padding"><?php the_title(); ?></h2>
+        </div>
+        <div class="content">
+            <?php the_content(); ?>
+        </div>
+        <?php endwhile; // end of the loop. ?>
+        </div>
     </div>
-    <div class="one-thirds">
-      <img src="https://image.shutterstock.com/image-photo/happy-cheerful-young-woman-wearing-260nw-613759379.jpg" alt="profile-pic">
-    </div>
-  </div>
+
 
   <div class="red-container">
     <div class="wrapper">
